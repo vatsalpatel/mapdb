04b8f758a052ceee0899987acb3853df391f04b0
Vatsal Patel <vatsalpatel.me@gmail.com>
2024-01-02 20:38:23 +0530

    feat: restore data from dump.rdb (#16)

--- core/engine.go
+++ core/engine.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"log"
+	"os"
+
 	"github.com/vatsalpatel/radish/store"
 )
 
@@ -19,9 +22,14 @@ type Item struct {
 }
 
 func NewEngine(storage store.Storer[*Item]) *Engine {
-	return &Engine{
+	engine := &Engine{
 		storage,
 	}
+	err := engine.load()
+	if err != nil {
+		log.Println(err)
+	}
+	return engine
 }
 
 func (e *Engine) Handle(input []byte) []byte {
@@ -38,4 +46,39 @@ func (e *Engine) Handle(input []byte) []byte {
 		return Serialize(err)
 	}
 	return Serialize(result)
+}
+
+func (e *Engine) load() error {
+	data, err := os.ReadFile("dump.rdb")
+	if err != nil {
+		return err
+	}
+
+	var key, value, expiry []byte
+	var current int
+
+	for i := 0; i < len(data); i++ {
+		switch {
+		case data[i] == ',':
+			current++
+		case data[i] == '\n':
+			_, err = e.execSet(string(key), string(value), string(expiry))
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			key, value, expiry = []byte{}, []byte{}, []byte{}
+			current = 0
+		default:
+			switch current {
+			case 0:
+				key = append(key, data[i])
+			case 1:
+				value = append(value, data[i])
+			case 2:
+				expiry = append(expiry, data[i])
+			}
+		}
+	}
+	return nil
 }
